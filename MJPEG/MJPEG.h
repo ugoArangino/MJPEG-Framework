720c023df11532403459b304fb7a4cca455cccf1
Ugo Arangino <ugo@arangino.de>
2015-03-14 23:52:55 +0100

    Initial Commit

---
+++ MJPEG/MJPEG.h
@@ -0,0 +1,19 @@
+//
+//  MJPEG.h
+//  MJPEG
+//
+//  Created by Ugo Arangino on 14.03.15.
+//  Copyright (c) 2015 Ugo Arangino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MJPEG.
+FOUNDATION_EXPORT double MJPEGVersionNumber;
+
+//! Project version string for MJPEG.
+FOUNDATION_EXPORT const unsigned char MJPEGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJPEG/PublicHeader.h>
+
+
